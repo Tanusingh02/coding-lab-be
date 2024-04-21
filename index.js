6ec0fe5d8c4c10a9a69b43191ecd31c472ad19c6
Tanusingh02 <tanu021202@gmail.com>
2024-04-21 22:23:45 +0530

    backend updates

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const  {generateFile} = require('./generateFile')
-const {executeCpp} = require('./executeCpp')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { generateFile } = require("./generateFile");
 
+const { addJobToQueue } = require("./jobQueue");
+const Job = require("./models/Job");
+
+mongoose.connect("mongodb://127.0.0.1/coding-lab"); 
+ mongoose.connection.on("error", (error) => {   console.error("Mongodb connection error:", error); });
+
+
+
+const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("/run",async(req,res)=>{
+app.post("/run", async (req, res) => {
+  const { language = "cpp", code } = req.body;
 
-    const { language = "cpp", code} = req.body;
-    if(code === undefined){
-        return res.status(400).json({success : false,error:"empty code body"});
-    }
-    try {
-    const  filePath = await generateFile(language,code);
-    const output = await executeCpp(filePath);
+  console.log(language, "Length:", code.length);
 
-    return res.json({filePath,output});
-    }
-    catch(err){
-        res.status(500).json({err});
-    }
-})
-app.listen(5000,()=>{
-    console.log('listening on port 5000');
+  if (code === undefined) {
+    return res.status(400).json({ success: false, error: "Empty code body!" });
+  }
+  // need to generate a c++ file with content from the request
+  const filepath = await generateFile(language, code);
+  // write into DB
+  const job = await new Job({ language, filepath }).save();
+  const jobId = job["_id"];
+  addJobToQueue(jobId);
+  res.status(201).json({ jobId });
+});
+
+app.get("/status", async (req, res) => {
+  const jobId = req.query.id;
+
+  if (jobId === undefined) {
+    return res
+      .status(400)
+      .json({ success: false, error: "missing id query param" });
+  }
+
+  const job = await Job.findById(jobId);
+
+  if (job === undefined) {
+    return res.status(400).json({ success: false, error: "couldn't find job" });
+  }
+
+  return res.status(200).json({ success: true, job });
+});
+
+app.listen(5000, () => {
+  console.log(`Listening on port 5000!`);
 });
